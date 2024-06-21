44feb6f06d5a92fdaf0c67e1fa90d917b171cf89
Rony Hanna <Rony.Hanna@outlook.com>
2024-06-21 18:15:47 +1200

    remove deprecated sorting layer system

--- BansheeEngine/BansheeEngine/source/graphics/Systems/MeshSystem.cpp
+++ BansheeEngine/BansheeEngine/source/graphics/Systems/MeshSystem.cpp
@@ -17,17 +17,7 @@ namespace Banshee
 		m_MeshRenderers.emplace_back(_component);
 	}
 
-	void MeshSystem::Update()
 	{
-		//SortByLayer();
-	}
 
-	void MeshSystem::SortByLayer()
-	{
-		//std::sort(m_MeshRenderers.begin(), m_MeshRenderers.end(), [](const std::shared_ptr<MeshComponent>& _c1, const std::shared_ptr<MeshComponent>& _c2)
-		//{
-		//	return std::tie(_c1->GetSortLayer().layerId, _c1->GetSortLayer().priority) <
-		//		   std::tie(_c2->GetSortLayer().layerId, _c2->GetSortLayer().priority);
-		//});
 	}
 } // End of Banshee namespace
