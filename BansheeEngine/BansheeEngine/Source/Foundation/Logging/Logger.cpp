dc4b9642d0c89610fb95bfe5e26d995c6bdc0ced
Rony Hanna <Rony.Hanna@outlook.com>
2024-07-30 13:05:34 +1200

    Refactor Logger (remove singleton)

--- BansheeEngine/BansheeEngine/Source/Foundation/Logging/Logger.cpp
+++ BansheeEngine/BansheeEngine/Source/Foundation/Logging/Logger.cpp
@@ -1,58 +1,37 @@
 #include "Logger.h"
-#include "Foundation/ResourceManager/ResourceManager.h"
-#include "Foundation/ResourceManager/File/FileManager.h"
-#include <stdarg.h>
 
 namespace Banshee
 {
-	#define STRINGIFY(text) #text
-	
-	constexpr auto g_Reset	= "\033[0m";
-	constexpr auto g_Red	= "\033[31m";
-	constexpr auto g_Green	= "\033[32m";
-	constexpr auto g_Yellow	= "\033[33m";
+	const Logger g_Logger;
 
-	Logger& Logger::Instance()
+	Logger::Logger()
+		: m_LogFile{}
 	{
-		static Logger s_Logger;
-		return s_Logger;
-	}
-
-	void Logger::PrintLog(const LogCategory _category, const char* _format, ...)
-	{
-		va_list argList = nullptr;
-		va_start(argList, _format);
-
-		const std::string categoryText = GetLogCategoryName(_category);
-
-		const uint16 size = 1024;
-		char userLog[size]{};
-		vsnprintf_s(userLog, size, _format, argList);
-		va_end(argList);
-
-		const std::string outputLog = categoryText + userLog;
-
-		switch (_category)
+		if (!m_LogFile.is_open())
 		{
-		case LogCategory::Trace:	printf("%s%s\n", g_Reset, outputLog.c_str()); break;
-		case LogCategory::Info:		printf("%s%s\n", g_Green, outputLog.c_str()); break;
-		case LogCategory::Warning:	printf("%s%s\n", g_Yellow, outputLog.c_str()); break;
-		case LogCategory::Error:	printf("%s%s\n", g_Red, outputLog.c_str()); break;
-		default:					printf("%s\n", outputLog.c_str());
+			m_LogFile.open("C:/Projects/Banshee-Engine/BansheeEngine/BansheeEngine/Banshee_Generated/logs.txt", std::ios::out | std::ios::app);
+			if (!m_LogFile)
+			{
+				throw std::runtime_error("ERROR: Failed to open log file.");
+			}
 		}
 
-		ResourceManager::Instance().GetFileManager()->WriteToLogFile(outputLog.c_str());
+		m_LogEvent = [this](const char* logData) { WriteToLogFile(logData); };
 	}
 
-	std::string Logger::GetLogCategoryName(const LogCategory _category)
+	Logger::~Logger()
 	{
-		switch (_category)
+		if (m_LogFile.is_open())
 		{
-		case LogCategory::Trace:	return STRINGIFY([TRACE]);
-		case LogCategory::Info:		return STRINGIFY([INFO]);
-		case LogCategory::Warning:	return STRINGIFY([WARNING]);
-		case LogCategory::Error:	return STRINGIFY([ERROR]);
-		default:					return "";
+			m_LogFile.close();
+		}
+	}
+
+	void Logger::WriteToLogFile(const char* _logData)
+	{
+		if (m_LogFile.is_open())
+		{
+			m_LogFile << _logData << '\n';
 		}
 	}
 } // End of Banshee namespace
